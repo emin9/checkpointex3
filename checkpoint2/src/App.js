ec043f3b6e9b4bdd5a2a39b3f563bdb65eb559da
emin9 <emsambiev@gmail.com>
2023-06-14 18:31:24 +0300

    First Commit
    First Commit

---
+++ checkpoint2/src/App.js
@@ -0,0 +1,11 @@
+import Base from "./base"
+
+function App(){
+    return(
+        <div>
+            <Base/>
+        </div>
+    )
+}
+
+export default App
---
+++ checkpoint2/src/base.jsx
@@ -0,0 +1,12 @@
+import logo from './logo192.png'
+
+function Base(){
+    return(
+        <div>
+            <h1>Hi</h1>
+            <img src={logo} alt="" />
+        </div>
+    )
+}
+
+export default Base
